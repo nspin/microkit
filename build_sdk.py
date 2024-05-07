d8a3da4e263c3d532a4ab771cf24308d977b6582
Nick Spinale <nick@nickspinale.com>
2024-05-07 14:35:22 -0400

    x

--- build_sdk.py
+++ build_sdk.py
@@ -212,7 +212,7 @@ def get_tool_target_triple() -> str:
         if host_arch == "x86_64":
             return "x86_64-unknown-linux-musl"
         elif host_arch == "aarch64":
-            return "aarch64-unknown-linux-musl"
+            return "aarch64-unknown-linux-gnu"
         else:
             raise Exception(f"Unexpected Linux architecture: {host_arch}")
     elif host_system == "Darwin":
